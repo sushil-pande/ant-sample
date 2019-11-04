91b35e26f2011bdd2491899e82fe8a3858f5f71f
sushil-pande <48666678+sushil-pande@users.noreply.github.com>
2019-11-04 17:16:23 +0530

    Update HelloWorld.java
    replaced the hello world message

--- src/piona/HelloWorld.java
+++ src/piona/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
      * HelloWorld constructor.
      */
     public HelloWorld() {
-        greetings = "Hello World!";
+        greetings = "Test Run for Intellipaat";
     }
 
     /**
